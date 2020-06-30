20aa8884bfe6a405799bafd3103ee2d0c705840d
Federico Katzaroff <fkatzaroff@gmail.com>
2020-06-30 20:56:06 -0300

    Set production api url

--- src/contexts/game-context.js
+++ src/contexts/game-context.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const GameContext = React.createContext({});
-
+const url = "https://ahorcado-g4.herokuapp.com/";
 const GameContextProvider = ({ children }) => {
   const [nick, setNick] = React.useState();
   const [error, setError] = React.useState();
   const [gameState, setGameState] = React.useState(null);
   const login = React.useCallback(async (newNick) => {
-    const response = await fetch("http://localhost:3000/" + newNick, {
+    const response = await fetch(url + newNick, {
       method: "POST",
     });
     const state = await response.json();
@@ -17,7 +17,7 @@ const GameContextProvider = ({ children }) => {
     } else setError(state.error);
   }, []);
   const startGame = React.useCallback(async () => {
-    const response = await fetch(`http://localhost:3000/${nick}/start`, {
+    const response = await fetch(`${url}${nick}/start`, {
       method: "POST",
     });
     const state = await response.json();
@@ -25,12 +25,9 @@ const GameContextProvider = ({ children }) => {
   }, [nick]);
   const sendLetter = React.useCallback(
     async (letter) => {
-      const response = await fetch(
-        `http://localhost:3000/${nick}/guess/${letter}`,
-        {
-          method: "POST",
-        }
-      );
+      const response = await fetch(`${url}${nick}/guess/${letter}`, {
+        method: "POST",
+      });
       const state = await response.json();
       setGameState(state);
     },
@@ -38,12 +35,9 @@ const GameContextProvider = ({ children }) => {
   );
   const sendWord = React.useCallback(
     async (word) => {
-      const response = await fetch(
-        `http://localhost:3000/${nick}/guessword/${word}`,
-        {
-          method: "POST",
-        }
-      );
+      const response = await fetch(`${url}${nick}/guessword/${word}`, {
+        method: "POST",
+      });
       const state = await response.json();
       setGameState(state);
     },
